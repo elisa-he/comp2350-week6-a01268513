984a1b4574a917aafd35617611a7865b9ae9e93f
elisa-he <97122439+elisa-he@users.noreply.github.com>
2022-02-20 16:10:33 -0800

    heroku updated

--- databaseConnection.js
+++ databaseConnection.js
@@ -3,11 +3,13 @@ const mysql = require('mysql2');
 const is_heroku = process.env.IS_HEROKU || false;
 
 const dbConfigHeroku = {
-	host: "us-cdbr-east-03.cleardb.com",
-	user: "b1ab7fb2ee03bc",
-	password: "2a484a2d",
-	database: "heroku_3d208ad4bd6f421",
-	multipleStatements: false
+	host: "eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+	user: "qh6w3j60pihz93no",
+	password: "xvhll4qonee7b2ba",
+	database: "uezpa201ev9kfuuq",
+	multipleStatements: false,
+	reconnect: true,
+};
 };
 
 const dbConfigLocal = {
