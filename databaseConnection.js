fe7e93aefb331da12a283ddf87d023ce601907ca
elisa <he.elisa1998@gmail.com>
2022-02-21 17:33:34 -0800

    data

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,32 +1,31 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const is_heroku = process.env.IS_HEROKU || false;
 
 const dbConfigHeroku = {
-	host: "eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-	user: "qh6w3j60pihz93no",
-	password: "xvhll4qonee7b2ba",
-	database: "uezpa201ev9kfuuq",
-	multipleStatements: false,
-	reconnect: true,
+  host: "eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "qh6w3j60pihz93no",
+  password: "xvhll4qonee7b2ba",
+  database: "uezpa201ev9kfuuq",
+  multipleStatements: false,
+  reconnect: true,
+  namePlaceholders: true,
 };
 
-
 const dbConfigLocal = {
-	host: "eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-	user: "qh6w3j60pihz93no",
-	password: "xvhll4qonee7b2ba",
-	database: "uezpa201ev9kfuuq",
-	multipleStatements: false,
-	reconnect: true,
+  host: "eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "qh6w3j60pihz93no",
+  password: "xvhll4qonee7b2ba",
+  database: "uezpa201ev9kfuuq",
+  multipleStatements: false,
+  reconnect: true,
+  namePlaceholders: true,
 };
 
 if (is_heroku) {
-	var database = mysql.createPool(dbConfigHeroku);
-}
-else {
-	var database = mysql.createPool(dbConfigLocal);
+  var database = mysql.createPool(dbConfigHeroku);
+} else {
+  var database = mysql.createPool(dbConfigLocal);
 }
 
 module.exports = database;
-		
